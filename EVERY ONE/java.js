cded41132996bd48bc2609ae897efbd4bebac393
azooz2005 <sony.sony2004aa@gmail.com>
2025-01-05 15:46:44 +0300

    Add files via upload

---
+++ EVERY ONE/java.js
@@ -0,0 +1,59 @@
+function red(){
+    var checkBox = document.getElementById("red");
+
+    var pink = document.getElementsByClassName("pink");
+  
+    if (checkBox.checked == true){
+       pink.overflow = "hidden";
+    } else {
+        pink.overflow = "visible";
+    }
+}
+function Pink(){
+    
+}
+function White(){
+   
+}
+function Purple(){
+    
+}
+function Green(){
+
+}
+function Twentyfive(){
+    
+}
+function thirty(){
+    
+}
+function Thirtyfive(){
+    
+}
+function forty(){
+    
+}
+function five(){
+    
+}
+function ten(){
+    
+}
+function fifteen(){
+    
+}
+function thirty(){
+    
+}
+function Flowers(){
+    
+}
+function Sweets(){
+    
+}
+function Teddy(){
+    
+}
+function Balloons(){
+    
+}
